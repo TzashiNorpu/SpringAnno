b86c4dd778c9e2a13438ac705a8850af83deedcc
TzashiNorpu <TzashiNorpu@gmail.com>
2020-08-25 18:10:56 +0800

    tututu

--- SpringMVCAnnotation/src/main/java/com/tz/MainTest.java
+++ SpringMVCAnnotation/src/main/java/com/tz/MainTest.java
@@ -8,12 +8,12 @@ import com.tz.bean.Person;
 import com.tz.config.MainConfig;
 
 public class MainTest {
-	
-	@SuppressWarnings("resource")
-	public static void main(String[] args) {
-//		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
-//		Person bean = (Person) applicationContext.getBean("person");
-//		System.out.println(bean);
+
+    @SuppressWarnings("resource")
+    public static void main(String[] args) {
+/*        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
+        Person bean = (Person) applicationContext.getBean("person");
+        System.out.println(bean);*/
 		
 		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(MainConfig.class);
 		Person bean = applicationContext.getBean(Person.class);
@@ -23,7 +23,7 @@ public class MainTest {
 		for (String name : namesForType) {
 			System.out.println(name);
 		}
-	
-	}
+
+    }
 
 }
--- SpringMVCAnnotation/src/main/java/com/tz/config/MainConfig.java
+++ SpringMVCAnnotation/src/main/java/com/tz/config/MainConfig.java
@@ -13,14 +13,14 @@ import com.tz.bean.Person;
 @Configuration  //告诉Spring这是一个配置类
 
 @ComponentScans(
-		value = {
-				@ComponentScan(value="com.tz",includeFilters = {
+        value = {
+                @ComponentScan(value = "com.tz", includeFilters = {
 /*						@Filter(type=FilterType.ANNOTATION,classes={Controller.class}),
 						@Filter(type=FilterType.ASSIGNABLE_TYPE,classes={BookService.class}),*/
-						@Filter(type=FilterType.CUSTOM,classes={MyTypeFilter.class})
-				},useDefaultFilters = false)	
-		}
-		)
+                        @Filter(type = FilterType.CUSTOM, classes = {MyTypeFilter.class})
+                }, useDefaultFilters = false)
+        }
+)
 //@ComponentScan  value:指定要扫描的包
 //excludeFilters = Filter[] ：指定扫描的时候按照什么规则排除那些组件
 //includeFilters = Filter[] ：指定扫描的时候只需要包含哪些组件
@@ -30,11 +30,11 @@ import com.tz.bean.Person;
 //FilterType.REGEX：使用正则指定
 //FilterType.CUSTOM：使用自定义规则
 public class MainConfig {
-	
-	//给容器中注册一个Bean;类型为返回值的类型，id默认是用方法名作为id
-	@Bean("person")
-	public Person person01(){
-		return new Person("lisi", 20);
-	}
+
+    //给容器中注册一个Bean;类型为返回值的类型，id默认是用方法名作为id
+    @Bean("person")
+    public Person person01() {
+        return new Person("lisi", 20, null);
+    }
 
 }
