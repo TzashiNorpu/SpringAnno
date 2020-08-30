a0bc9377e5d8743c4f62f490c3692d7edf7961b4
tzashinorpu <tzashinorpu@google.com>
2020-08-30 23:05:42 +0800

    tututu

--- SpringMVCAnnotation/src/test/java/com/tz/test/IOCTest_AOP.java
+++ SpringMVCAnnotation/src/test/java/com/tz/test/IOCTest_AOP.java
@@ -25,7 +25,7 @@ public class IOCTest_AOP {
 //		从容器中获取的 bean 才有 切面功能
 		MathCalculator mathCalculator = applicationContext.getBean(MathCalculator.class);
 		
-		mathCalculator.div(1, 0);
+		mathCalculator.div(1,1);
 		
 		applicationContext.close();
 	}
